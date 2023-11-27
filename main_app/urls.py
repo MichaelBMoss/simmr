c7ae9edde13e346a853edc94fac0fb1b289b4f96
MichaelBMoss <michaelbmoss@gmail.com>
2023-11-27 07:28:31 -0800

    intiate profile branch

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('recipes/<int:pk>/', views.RecipeDetailView.as_view(), name='recipe_detail'),
     path('recipes/<int:pk>/update/', views.RecipeUpdateView.as_view(), name='recipes_update'),
     path('recipes/<int:pk>/delete/', views.RecipeDeleteView.as_view(), name='recipes_delete'),
+    path('profile/<int:pk>/', views.user_profile, name='profile'),
 ]
