59760f8b91b0b6ad60f356379a988ce8192c56a7
ianmacdonald <ianpetercreative@gmail.com>
2023-11-30 13:59:09 -0700

    revert

--- simmr/settings.py
+++ simmr/settings.py
@@ -19,8 +19,6 @@ import environ
 environ.Env()
 environ.Env.read_env()
 
-django_heroku.settings(locals())
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
