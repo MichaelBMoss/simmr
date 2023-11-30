c2e4eea8631cf2132b7d53a7ab616f94f9055cfd
ianmacdonald <ianpetercreative@gmail.com>
2023-11-30 14:21:07 -0700

    move django_heroku to bottom of settings.py

--- simmr/settings.py
+++ simmr/settings.py
@@ -17,7 +17,6 @@ import environ
 
 environ.Env()
 environ.Env.read_env()
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,3 +132,4 @@ LOGOUT_REDIRECT_URL = '/'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
